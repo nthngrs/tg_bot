e6ce21e442c7c2fcbdb4f156599866cd58b27cbb
nthngrs <rshncam@yandex.ru>
2023-06-01 17:49:38 +0300

    second commit

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,12 +1,10 @@
 from aiogram import Router
 from aiogram.types import Message
-from lexicon.lexicon import LEXICON_RU
+from lexicon.lexicon_ru import LEXICON_RU
 
 router: Router = Router()
 
+
 @router.message()
-async def send_echo(message: Message):
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(text=LEXICON_RU['no_echo'])
+async def send_answer(message: Message):
+    await message.answer(text=LEXICON_RU['other_answer'])
